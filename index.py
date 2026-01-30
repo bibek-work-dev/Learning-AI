7c0112bf0b3c1720b7a9445eb59e4b8fc0afa70d
Bibek Koirala <bibek.koirala@ebpearls.com>
2026-01-30 17:49:41 +0545

    done

---
+++ index.py
@@ -0,0 +1,8 @@
+from langchain.chat_models import HuggingFaceHub
+import os
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+os.environ["HUGGINGFACE_API_TOKEN"] =  "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"
+
